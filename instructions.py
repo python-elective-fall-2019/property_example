c76e069d68ec95dc248a96fb0551a592d35b629e
clbokea <clbo@kea.dk>
2019-10-04 15:30:00 +0200

    instructions

--- instructions.py
+++ instructions.py
@@ -1,13 +1,17 @@
 # instructions.py
 
 print(""" 
-To use this module: 
-create an object of the triangle class 
+Thank you for cloning my module!  
+The module can calculate the hypotenuse of a right angled triangle.
+
+To use this:    
+Create an object of the triangle class 
     t = Triangle()
 
 
 The triangles 3 sides are called a, b, c.  
-a and b can be set by typing t.a = 20 and t.b = 10
-t.c will give you a calculated value (the lenght of c).
+a and b can be set by typing t.a = 20 and t.b = 10  
+You can see the values of a and b by typing t.a and t.b  
+Typing t.c will give you a calculated value of c.
 
 """)
--- property_example.py
+++ property_example.py
@@ -3,9 +3,9 @@ import math
 import instructions
 
 class Triangle:
+
     def __init__(self):
         self.a = 30
         self.b = 30
-        self.c = math.sqrt(self.a**2 + self.b**2)
-
-
+        self.__c = math.sqrt(self.a**2 + self.b**2)
+    
